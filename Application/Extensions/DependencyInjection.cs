419113b53900a294ff3aced0fe5f52695e7547ce
ahmedzakaria432 <ahmed.zakaria432@outlook.com>
2022-04-17 16:58:57 +0200

    Adding Jwt

--- Application/Extensions/DependencyInjection.cs
+++ Application/Extensions/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,12 @@ namespace Application.Extensions
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services) 
+        {
+            
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
     }
 }
--- Persistance/DependencyInjection.cs
+++ Persistance/DependencyInjection.cs
@@ -6,15 +6,27 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using System.Reflection;
+using Infrastructure.Infrastructure.Helpers;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Infrastructure.Peresistence.Data;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services) 
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration ) 
         {
 
-            
+            services.Configure<Jwt>(cfg => configuration.GetSection("JWT"));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options=>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            );
+
 
             return services;
         }
